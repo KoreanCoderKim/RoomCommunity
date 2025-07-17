472698889779033db76f3bb132e6aead5e5c4478
KimJY <sagwai141@gmail.com>
2025-07-17 09:07:46 +0900

    ArticleRepository.java 업데이트

--- Article/src/main/java/com/example/Article/ArticleRepository.java
+++ Article/src/main/java/com/example/Article/ArticleRepository.java
@@ -3,5 +3,5 @@ package com.example.Article;
 import org.springframework.data.repository.CrudRepository;
 import com.example.Article.ArticleDto;
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+     public Article findByRoomId();
 }
